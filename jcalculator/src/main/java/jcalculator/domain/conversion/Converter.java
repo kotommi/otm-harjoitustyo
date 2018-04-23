0add7031fca94c4bb580f7e78df7a241382a1b94
kotommi <tommi.koivula@helsinki.fi>
2018-04-23 21:46:21 +0300

    javadocs for converter

--- jcalculator/src/main/java/jcalculator/domain/conversion/Converter.java
+++ jcalculator/src/main/java/jcalculator/domain/conversion/Converter.java
@@ -1,28 +1,58 @@
 package jcalculator.domain.conversion;
 
+/**
+ * Class for converting numbers from one base to another.
+ *
+ * @author tomko
+ */
 public class Converter {
+
     private Encoding from;
     private Encoding to;
     private Endian endian;
 
+    /**
+     * Default constructor that sets the values to the GUI defaults
+     */
     public Converter() {
         this.from = Encoding.DECIMAL;
         this.to = Encoding.BINARY;
         this.endian = Endian.LITTLE;
     }
 
+    /**
+     * Sets endian.
+     *
+     * @param endian
+     */
     public void setEndian(Endian endian) {
         this.endian = endian;
     }
 
+    /**
+     * Sets the starting encoding.
+     *
+     * @param from Value of the enum Encoding.
+     */
     public void setFrom(Encoding from) {
         this.from = from;
     }
 
+    /**
+     * Sets the base to encode to.
+     *
+     * @param to Value of the enum Encoding.
+     */
     public void setTo(Encoding to) {
         this.to = to;
     }
 
+    /**
+     * Converts a number from a base to another according to the settings.
+     *
+     * @param input A String representing a numerical value.
+     * @return A String in the base represented by Encoding to.
+     */
     public String convert(String input) {
         input = input.trim();
         switch (from) {
@@ -31,11 +61,17 @@ public class Converter {
             case HEX:
                 return parseHex(input);
             case BINARY:
-                break;
+                return parseBinary(input);
         }
         return input;
     }
 
+    /**
+     * Converts an int value to a String according to the Encoding to.
+     *
+     * @param input integer to convert.
+     * @return String formatted to a given base.
+     */
     private String convertInt(int input) {
         if (endian == Endian.BIG) {
             input = reverseEndian(input);
@@ -51,6 +87,12 @@ public class Converter {
         return "";
     }
 
+    /**
+     * Parses a hexadecimal String to an int.
+     *
+     * @param input see convert().
+     * @return
+     */
     private String parseHex(String input) {
         int parse;
         try {
@@ -65,6 +107,12 @@ public class Converter {
         return convertInt(parse);
     }
 
+    /**
+     * Parses a decimal String to an int.
+     *
+     * @param input see convert().
+     * @return
+     */
     private String parseDecimal(String input) {
         int parse;
         try {
@@ -75,19 +123,57 @@ public class Converter {
         return convertInt(parse);
     }
 
+    /**
+     * Parses a binary String to an int.
+     *
+     * @param input see convert().
+     * @return
+     */
+    private String parseBinary(String input) {
+        int parse;
+        try {
+            parse = Integer.parseInt(input, 2);
+        } catch (NumberFormatException e) {
+            return "NaN";
+        }
+        return convertInt(parse);
+    }
+
+    /**
+     * Flips the endianness of an int.
+     *
+     * @param i an int to convert.
+     * @return An int in opposite endian format.
+     */
     private int reverseEndian(int i) {
         return (i & 0xff) << 24 | (i & 0xff00) << 8 | (i & 0xff0000) >> 8 | (i >> 24) & 0xff;
     }
 
+    /**
+     * Getter for Encoding from.
+     *
+     * @return
+     */
     public Encoding getFrom() {
         return from;
     }
 
+    /**
+     * Getter for Encoding to.
+     *
+     * @return
+     */
     public Encoding getTo() {
         return to;
     }
 
+    /**
+     * Getter for Endian.
+     *
+     * @return
+     */
     public Endian getEndian() {
         return endian;
     }
+
 }
